02019a5b2e301e0cfaaf10a9a07618114798c5d7
Chetan baliyan <chetanbaliyan10@gmail.com>
2022-09-16 17:13:29 +0530

    Update Details.js

--- src/components/steps/Details.js
+++ src/components/steps/Details.js
@@ -23,21 +23,7 @@ export default function Details() {
           />
         </div>
       </div>
-      <div className="w-full mx-2 flex-1">
-        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
-          Pin code
-        </div>
-        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
-          <input
-            onChange={handleChange}
-            value={userData["city"] || ""}
-            name="city"
-            placeholder="Enter your pincode"
-            type="text"
-            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
-          />
-        </div>
-      </div>
+    
       <div className="w-full mx-2 flex-1">
         <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
           City
