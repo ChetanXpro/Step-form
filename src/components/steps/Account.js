98ddbeabbbaee44e8af350ee502b31142600cde0
Chetan baliyan <chetanbaliyan10@gmail.com>
2022-09-16 17:10:18 +0530

    Update Account.js

--- src/components/steps/Account.js
+++ src/components/steps/Account.js
@@ -41,21 +41,7 @@ export default function Account() {
           />
         </div>
       </div>
-      <div className="mx-2 w-full flex-1">
-        <div className="mt-3 h-6 text-xs font-bold uppercase leading-8 text-gray-500">
-          Confirm Password
-        </div>
-        <div className="my-2 flex rounded border border-gray-200 bg-white p-1">
-          <input
-            onChange={handleChange}
-            value={userData["password"] || ""}
-            name="password"
-            placeholder="Confirm Password"
-            type="password"
-            className="w-full appearance-none p-1 px-2 text-gray-800 outline-none"
-          />
-        </div>
-      </div>
+      
     </div>
   );
 }
