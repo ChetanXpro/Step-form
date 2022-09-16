b0f4fd69867662e48976651ed2dd295e53a52488
Chetan baliyan <chetanbaliyan10@gmail.com>
2022-09-16 17:10:51 +0530

    Update Payment.js

--- src/components/steps/Payment.js
+++ src/components/steps/Payment.js
@@ -40,21 +40,7 @@ export default function Payment() {
           />
         </div>
       </div>
-      <div className="w-full mx-2 flex-1">
-        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
-          CVV
-        </div>
-        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
-          <input
-            onChange={handleChange}
-            value={userData["exp"] || ""}
-            name="exp"
-            placeholder="***"
-            type="text"
-            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
-          />
-        </div>
-      </div>
+      
     </div>
   );
 }
